00135053d44f9c4f5be551f2d519ff0d4e88c8e9
Ritesh Kaushal <riteshkaushal57@gmail.com>
2026-02-14 11:45:14 +0530

    cc

---
+++ EKS/modules/vpc/main.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
